54a9e29c6313fb1f0f81050285a34f00ca9e1667
Ember Baker <erb325@gmail.com>
2015-11-05 17:19:51 -0500

    Making the request and saving the returned information to a dictionary.

---
+++ EmployeeTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EmployeeTableViewController.h
+//  SlackInterview
+//
+//  Created by Ember Baker on 11/5/15.
+//  Copyright (c) 2015 Ember Baker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EmployeeTableViewController : UITableViewController
+
+@end
---
+++ EmployeeTableViewController.m
@@ -0,0 +1,127 @@
+//
+//  EmployeeTableViewController.m
+//  SlackInterview
+//
+//  Created by Ember Baker on 11/5/15.
+//  Copyright (c) 2015 Ember Baker. All rights reserved.
+//
+
+#import "EmployeeTableViewController.h"
+
+@interface EmployeeTableViewController ()
+
+@end
+
+@implementation EmployeeTableViewController{
+    NSString *token;
+    NSDictionary *employeeData;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self getEmployeeInformation];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+}
+-(void)getEmployeeInformation {
+    NSString *requestURL = @"https://slack.com/api/users.list?token=xoxp-4698769766-4698769768-4898023905-7a1afa";
+    
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:requestURL]];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue mainQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+        
+        if (connectionError == nil) {
+            NSLog(@"Data saved");
+            NSError* error;
+            employeeData = [NSJSONSerialization JSONObjectWithData:data options:kNilOptions error:&error];
+            [self saveData];
+        }
+        
+    }];
+}
+-(void)saveData{
+    NSLog(@"Data: %@", employeeData.description);
+    
+    //insert core data text here 
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+#warning Potentially incomplete method implementation.
+    // Return the number of sections.
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
